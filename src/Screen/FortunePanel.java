20c23ceb050fd889dcf94501fa99b4c62a8c0211
Monica Zhang <108095408+monica410@users.noreply.github.com>
2022-07-16 13:39:14 -0700

    fortune

--- src/Screen/FortunePanel.java
+++ src/Screen/FortunePanel.java
@@ -1,7 +1,24 @@
 package Screen;
 
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
 import javax.swing.*;
 
 public class FortunePanel extends JPanel{
 
+	public FortunePanel() {
+		BufferedImage background;
+		try {
+			background = ImageIO.read(new File("fortune.png"));
+			JLabel picLabel = new JLabel(new ImageIcon(background));
+			add(picLabel);
+		} catch (IOException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+	}
+	
 }
