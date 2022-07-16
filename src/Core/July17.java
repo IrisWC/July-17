895cd7391371dd2e0696a09d74435919f08cb6bf
Iris Chou <iris.w.chou@gmail.com>
2022-07-16 13:50:20 -0700

    More panel switching testing

--- src/Core/July17.java
+++ src/Core/July17.java
@@ -8,43 +8,82 @@ import Screen.*;
 public class July17 extends JFrame implements ActionListener {
 	
 	CardLayout cl;
-    JPanel mainPanel;
-    MenuPanel menu;
-    CalculatorPanel calculator;
-    FortunePanel fortune;
+	Container container;
+	JPanel panel1, panel2;
+	JButton button1, button2;
 	
 	public July17() {
+		super("July 17");
 		cl = new CardLayout();
-		mainPanel = new JPanel(cl);
-		menu = new MenuPanel();
-		calculator = new CalculatorPanel();
-		fortune = new FortunePanel();
+		container = getContentPane();
+		container.setLayout(cl);
 		
-//		JButton back = new JButton("Go back");
-//		back.addActionListener(this);
-//		
-		mainPanel.add(menu, "menu");
-		mainPanel.add(calculator, "calculator");
-		mainPanel.add(fortune, "fortune");
+		panel1 = new JPanel();
+		panel2 = new JPanel();
 		
-		add(mainPanel);
-		add(menu);
-		add(calculator);
-		setSize(1600,900);
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setVisible(true);
+		button1 = new JButton("Go to panel 2");
+		button2 = new JButton("Go to panel 1");
+		
+		button1.addActionListener(this);
+		button2.addActionListener(this);
+		
+		panel1.add(button1);
+		panel2.add(button2);
+		
+		container.add(panel1, "panel1");
+		container.add(panel2, "panel2");
+		
+
 	}
 	
+	@Override
 	public void actionPerformed(ActionEvent e) {
-		cl.show(mainPanel, "menu");
+		if(e.getSource() == button1) {
+			cl.show(container, "panel2");
+		}
+		if(e.getSource() == button2) {
+			cl.show(container, "panel1");
+		}
 	}
 	
-	public static void main(String[] args) {
-        SwingUtilities.invokeLater(new Runnable(){
-            @Override
-            public void run() {
-                July17 game = new July17();
-            }
-        });
-    }
+//	CardLayout cl;
+//    JPanel mainPanel;
+//    MenuPanel menu;
+//    CalculatorPanel calculator;
+//    FortunePanel fortune;
+//	
+//	public July17() {
+//		cl = new CardLayout();
+//		mainPanel = new JPanel(cl);
+//		menu = new MenuPanel();
+//		calculator = new CalculatorPanel();
+//		fortune = new FortunePanel();
+//		
+////		JButton back = new JButton("Go back");
+////		back.addActionListener(this);
+////		
+//		mainPanel.add(menu, "menu");
+//		mainPanel.add(calculator, "calculator");
+//		mainPanel.add(fortune, "fortune");
+//		
+//		add(mainPanel);
+//		add(menu);
+//		add(calculator);
+//		setSize(1600,900);
+//		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+//		setVisible(true);
+//	}
+//	
+//	public void actionPerformed(ActionEvent e) {
+//		cl.show(mainPanel, "menu");
+//	}
+//	
+//	public static void main(String[] args) {
+//        SwingUtilities.invokeLater(new Runnable(){
+//            @Override
+//            public void run() {
+//                July17 game = new July17();
+//            }
+//        });
+//    }
 }
--- src/Core/Main.java
+++ src/Core/Main.java
@@ -7,13 +7,13 @@ import Screen.*;
 
 public class Main {
 	public static void main(String args[]) {
-		JFrame window = new JFrame("July 17");
+		July17 window = new July17();
 		window.setSize(1600, 900);
 	    window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	    
-	    CalculatorPanel calculator = new CalculatorPanel();
-	    Container c = window.getContentPane();
-	    c.add(calculator);
+//	    CalculatorPanel calculator = new CalculatorPanel();
+//	    Container c = window.getContentPane();
+//	    c.add(calculator);
 
 	    window.setVisible(true);
 	    
