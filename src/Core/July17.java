e52b9981da0816604499c40252547df3731faefc
Iris Chou <iris.w.chou@gmail.com>
2022-07-16 09:14:55 -0700

    Made window pop-up

--- src/Core/July17.java
+++ src/Core/July17.java
@@ -1,5 +1,11 @@
 package Core;
 
-public class July17 {
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
 
+public class July17 extends JPanel {
+	public July17() {
+		
+	}
 }
--- src/Core/Main.java
+++ src/Core/Main.java
@@ -1,7 +1,20 @@
 package Core;
 
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+
 public class Main {
 	public static void main(String args[]) {
-		System.out.println("Hello World");
+		JFrame window = new JFrame("July 17");
+		window.setSize(800, 450);
+	    window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	    
+	    July17 panel = new July17();
+	    panel.setBackground(Color.WHITE);
+	    Container c = window.getContentPane();
+	    c.add(panel);
+
+	    window.setVisible(true);
 	}
 }
