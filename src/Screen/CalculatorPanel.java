acb22831b4ec501438a5b681f5336edc891ce39e
Iris Chou <iris.w.chou@gmail.com>
2022-07-16 10:26:33 -0700

    Added button icon

--- src/Screen/CalculatorPanel.java
+++ src/Screen/CalculatorPanel.java
@@ -3,10 +3,16 @@ package Screen;
 import java.awt.Color;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-
 import javax.swing.*;
+import javax.imageio.ImageIO;
+import java.awt.Graphics;
+import java.awt.image.BufferedImage;
+import java.io.File;
 
 public class CalculatorPanel extends JPanel{
+	
+	private BufferedImage image;
+	
 	public CalculatorPanel() {
 		setBackground(Color.WHITE);
 		
@@ -15,22 +21,32 @@ public class CalculatorPanel extends JPanel{
 		JComboBox<String> jComboBox = new JComboBox<>(optionsToChoose);
         jComboBox.setBounds(80, 50, 140, 20);
         
-        JButton jButton = new JButton("Done");
+        ImageIcon icon = new ImageIcon("Test.png");
+        
+        JButton jButton = new JButton(icon);
         jButton.setBounds(100, 100, 90, 20);
 
-        JLabel jLabel = new JLabel();
-        jLabel.setBounds(90, 100, 400, 100);
+//        JLabel jLabel = new JLabel();
+//        jLabel.setBounds(90, 100, 400, 100);
+        
+       //image = ImageIO.read(new File("image name and path"));
         
         add(jButton);
         add(jComboBox);
-        add(jLabel);
+        //add(jLabel);
         
         jButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
                 String selectedFruit = "You selected " + jComboBox.getItemAt(jComboBox.getSelectedIndex());
-                jLabel.setText(selectedFruit);
+                //jLabel.setText(selectedFruit);
             }
         });
 	}
+	
+//	public void paintComponent(Graphics g) {
+//        super.paintComponent(g);
+//        g.drawImage(image, 0, 0, this);       
+//    }
 }
+
