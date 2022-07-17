5c0d73fec44f5756e44f0fe5b170b922f1cfdf97
Monica Zhang <108095408+monica410@users.noreply.github.com>
2022-07-16 20:26:34 -0700

    menu pic

--- src/Screen/MenuPanel.java
+++ src/Screen/MenuPanel.java
@@ -3,33 +3,36 @@ package Screen;
 import javax.swing.*;
 
 import java.awt.Color;
+import java.awt.Image;
 import java.awt.color.*;
 import java.awt.event.ActionListener;
 
 public class MenuPanel extends JPanel{
 	
 	public MenuPanel(ActionListener a) {
-		ImageIcon menuBackground = new ImageIcon("img//menu.png"); //TODO link to correct background file
+		ImageIcon menuBackground = new ImageIcon("img//menu.gif"); //TODO link to correct background file
 		JLabel menuLabel = new JLabel(menuBackground);
 		menuLabel.setSize(1600, 900);
 		
-		ImageIcon nwcIcon = new ImageIcon("img//calculateNW.png"); //TODO link to correct button image
+		ImageIcon nwcIcon = new ImageIcon("img//nwc.png"); //TODO link to correct button image
 	    JButton goCalculator = new JButton(nwcIcon);
 	    goCalculator.setBounds(675, 737, 250, 90);
 	    goCalculator.setBackground(Color.BLACK);
 	    goCalculator.setBorderPainted(true);
 	    
-	    ImageIcon fortuneIcon = new ImageIcon("img//calculateNW.png"); //TODO link to correct button image
+	    ImageIcon fortuneIcon = new ImageIcon("img//ft.png"); //TODO link to correct button image
 	    JButton goFortune = new JButton(fortuneIcon);
 	    goFortune.setBounds(675, 737, 250, 90);
 	    goFortune.setBackground(Color.BLACK);
 	    goFortune.setBorderPainted(true);
 	    
-	    ImageIcon infoIcon = new ImageIcon("img//calculateNW.png"); //TODO link to correct button image
+	    ImageIcon infoIcon = new ImageIcon("img//i.png"); //TODO link to correct button image
 	    JButton goInfo = new JButton(infoIcon);
 	    goInfo.setBounds(675, 737, 250, 90);
 	    goInfo.setBackground(Color.BLACK);
 	    goInfo.setBorderPainted(true);
+	    
+	    
 	}
 
 }
