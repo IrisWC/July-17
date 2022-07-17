51265a370b3fef3b43f4dc2b763df380eac998ef
Iris Chou <iris.w.chou@gmail.com>
2022-07-16 17:44:58 -0700

    Update CalculatorPanel.java

--- src/Screen/CalculatorPanel.java
+++ src/Screen/CalculatorPanel.java
@@ -101,11 +101,11 @@ public class CalculatorPanel extends JPanel{
 	    label.add(lotteryBox);
 	    label.add(holidayBox);
 	    
-	    ImageIcon icon = new ImageIcon("img//caculateNW.png");
+//	    ImageIcon icon = new ImageIcon("img//caculateNW.png");
 	    
-	    JButton jButton = new JButton(icon);
-	    jButton.setBounds(100, 100, 90, 20);
-	    jButton.setBackground(new Color(40, 75, 80));
+//	    JButton jButton = new JButton(icon);
+//	    jButton.setBounds(100, 100, 90, 20);
+//	    jButton.setBackground(new Color(40, 75, 80));
 //	    jButton.setBorderPainted(false);
 //	
 //	//        JLabel jLabel = new JLabel();
